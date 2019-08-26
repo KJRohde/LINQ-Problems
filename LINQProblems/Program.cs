5d0f6ce4f01037bcf4ba54d39a807e9afd4cafa1
Kyle Rohde <krohde13@gmail.com>
2019-08-26 10:05:44 -0500

    first two problems complete

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -10,6 +10,25 @@ namespace LINQProblems
     {
         static void Main(string[] args)
         {
+            //filter list to only include strings that contain "th"
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsTH = words.Where(w => w.Contains("th"));
+            foreach (string word in wordsTH)
+            Console.WriteLine(word);
+            Console.ReadLine();
+
+            //return list with no duplicates
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var namesDistinct = names.Distinct();
+            foreach (string name in namesDistinct)
+                Console.WriteLine(name);
+            Console.ReadLine();
+
+            //remove each students lowest grade, find average per student, find average of averages
+            List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+
+            //Write function to return each letter alphabetically and with how many instances of that letter are in the string
+
         }
     }
 }
