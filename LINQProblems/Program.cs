f20bc1b9dc542a6846fb35e52e102edf228af9a5
Kyle Rohde <krohde13@gmail.com>
2019-08-26 14:31:23 -0500

    taking care of merge conflicts

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -11,80 +11,64 @@ namespace LINQProblems
         static void Main(string[] args)
         {
             ////filter list to only include strings that contain "th"
-            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            //var wordsTH = words.Where(w => w.Contains("th"));
-            //foreach (string word in wordsTH)
-            //Console.WriteLine(word);
-            //Console.ReadLine();
+            List<string> testOne = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics", "tether" };
+            void FindTH(List<string> words)
+            {
+                var wordsTH = words.Where(w => w.Contains("th"));
+                foreach (string word in wordsTH)
+                    Console.WriteLine(word);
+                Console.ReadLine();
+            }
+            FindTH(testOne);
 
             ////return list with no duplicates
-            //List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
-            //var namesDistinct = names.Distinct();
-            //foreach (string name in namesDistinct)
-            //    Console.WriteLine(name);
-            //Console.ReadLine();
+            List<string> testTwo = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            void RemoveDuplicates(List<string> names)
+            {
+                var namesDistinct = names.Distinct();
+                foreach (string name in namesDistinct)
+                    Console.WriteLine(name);
+                Console.ReadLine();
+            }
+            RemoveDuplicates(testTwo);
 
-            //remove each students lowest grade, find average per student, find average of averages
+            ////remove each students lowest grade, find average per student, find average of averages
             List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
             char[] separator = { ',' };
             List<double> classAverage = new List<double>();
-            //Student 1
-            var studentOne = classGrades[0];
-            string[] studentOneList = studentOne.Split(separator);
-            List<int> studentOneInt = new List<int>();
-            foreach (string s in studentOneList)
+            void GetClassAverage(List<string> myClass)
             {
-                studentOneInt.Add(int.Parse(s));
+                for (int i = 0; i < myClass.Count; i++)
+                {
+                    var student = myClass[i];
+                    string[] studentList = student.Split(separator);
+                    List<int> studentInt = new List<int>();
+                    foreach (string s in studentList)
+                    {
+                        studentInt.Add(int.Parse(s));
+                    }
+                    studentInt.Remove(studentInt.Min());
+                    double studentAverage = studentInt.Average();
+                    classAverage.Add(studentAverage);
+                    Console.WriteLine("Student " + (i + 1) + " has an average of: " + studentAverage);
+                }
+                double wholeClassAverage = classAverage.Average();
+
+
+                Console.WriteLine("This classes average is: " + wholeClassAverage);
+                Console.ReadLine();
             }
-            studentOneInt.Remove(studentOneInt.Min());
-            double studentOneAverage = studentOneInt.Average();
-            classAverage.Add(studentOneAverage);
 
-            //Student 2
-            var studentTwo = classGrades[1];
-            string[] studentTwoList = studentTwo.Split(separator);
-            List<int> studentTwoInt = new List<int>();
-            foreach (string s in studentTwoList)
-            {
-                studentTwoInt.Add(int.Parse(s));
-            }
-            studentTwoInt.Remove(studentTwoInt.Min());
-            double studentTwoAverage = studentTwoInt.Average();
-            classAverage.Add(studentTwoAverage);
+            GetClassAverage(classGrades);
 
-            //Student 3
-            var studentThree = classGrades[2];
-            string[] studentThreeList = studentThree.Split(separator);
-            List<int> studentThreeInt = new List<int>();
-            foreach (string s in studentThreeList)
-            {
-                studentThreeInt.Add(int.Parse(s));
-            }
-            studentThreeInt.Remove(studentThreeInt.Min());
-            double studentThreeAverage = studentThreeInt.Average();
-            classAverage.Add(studentThreeAverage);
-
-            //Student 4
-            var studentFour = classGrades[3];
-            string[] studentFourList = studentFour.Split(separator);
-            List<int> studentFourInt = new List<int>();
-            foreach (string s in studentFourList)
-            {
-                studentFourInt.Add(int.Parse(s));
-            }
-            studentFourInt.Remove(studentFourInt.Min());
-            double studentFourAverage = studentFourInt.Average();
-            classAverage.Add(studentFourAverage);
-
-            double wholeClassAverage = classAverage.Average();
-
-
-            Console.WriteLine(wholeClassAverage);
-            Console.ReadLine();
-            
 
             //Write function to return each letter alphabetically and with how many instances of that letter are in the string
-
+            //Take in a string from user
+            //1. split the string into individual letters
+            //2. for each letter, search the list for repeat instances and count them
+            //3. remove the recurring strings as they are counted
+            //4. add number at the end of the letter string
+            //5. bring the list together to form one string
         }
     }
 }
